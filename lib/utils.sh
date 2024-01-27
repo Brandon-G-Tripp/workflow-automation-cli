2c495273296e2804efb9b7638707ae63f08f13d5
Brandon Tripp <brandongtripp@gmail.com>
2024-01-26 19:28:20 -0700

    Adding the print function to utils and validate params

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,24 +1,30 @@
+#!/bin/bash
+
+. ./lib/shflags/shflags
+
 print_help() {
-    cat <<- EOF
-    Usage: cli.sh [options] <workflow>
+    cat <<EOF
+Usage: cli.sh [options] <workflow>
 
-    Options: 
-        -h, --help  Print this help message
-        -v, --version  Print version
-        -V, --verbose  Enable verbose output 
-        -w, --workflow  The workflow to execute
+Options: 
+    -h, --help  Print this help message
+    -v, --version  Print version
+    -V, --verbose  Enable verbose output 
+    -w, --workflow  The workflow to execute
 
 
-    Workflows:
-        example  Description of example workflow 
-        backup  Description of backup workflow 
-        
-    EOF
+Workflows:
+    example  Description of example workflow 
+    backup  Description of backup workflow 
+    
+EOF
 } 
 
 validate_params() {
-    if [ -z "$1" ]; then 
-        echo "Error: $2 is required" >&2
+    local arg_name=$1
+    local error_msg=$2
+    if [ -z "$arg_name" ]; then 
+        echo "Error: $error_msg is required" >&2
         print_help
         exit 1
     fi
