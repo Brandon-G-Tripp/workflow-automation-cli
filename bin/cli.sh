6ff60ddafab63db6cf237922ce70c231da129856
Brandon Tripp <brandongtripp@gmail.com>
2024-01-31 19:01:36 -0700

    WIP: Set up to use while statement with getopts since shflags is not working

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,41 +1,75 @@
 #!/bin/bash
-# cli.sh
-#
-. ./lib/shflags/shflags
-. ./lib/utils.sh
+# source /Users/brandontripp/projects-software/personal/dataano-programming-learning-projects/scripting-projects/workflow-automation-cli/lib/shflags/shflags
+# source /Users/brandontripp/projects-software/personal/dataano-programming-learning-projects/scripting-projects/workflow-automation-cli/lib/utils.sh
+source ./lib/utils.sh
 
-# Define options to parse
-OPTIONS=(
-    "--help, -h:Show usage information"
-    "--version, -v:Print version"
-    "--verbose, -V:Enable verbose logging"
-    "--workflow=-w:The workflow to execute"
-) 
+# Define long option names
+LONGOPTS="help,version,verbose,workflow:"
 
-# Parse command line args
-FLAGS=$(shflags ${OPTIONS[@]} $@)
+# before getopts
+echo "Before getopt"
 
+# Parse options
+ARGS=$(getopt -o "hVv:w:" --long "$LONGOPTS" -n "$0" -- "$@")
+eval set -- "$ARGS"
 
-# Check for help flag 
-if [ ${FLAGS[h]} ]; then
-    echo -n "Usage: cli.sh <workflow_name> [options]" 
-    exit 0 
+while true; do 
+    case $1 in
+        -h|--help)
+            #print exact help output 
+            echo -e "Help output: $output"
+
+            # print script name substitution
+            echo "Script name: $0"
+
+            # hard code expected string
+            echo -e "Usage: cli.sh [-h] [-V] [-v] [-w <workflow>]"
+            exit 0
+            ;;
+        -V|--version)
+            echo -e "workflow v0.1.0"
+            exit 0 
+            ;;
+        -v|--verbose)
+            verbose=true
+            shift
+            ;;
+        -w|--workflow)
+            workflow="$2"
+            shift 2
+            ;;
+        --)
+            shift 
+            break
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
+done
+
+# after getopts
+echo "After getopt"
+
+workflow=${workflow:-example}
+
+if [ "$verbose" == true ]; then
+    echo -e "Running ${workflow} workflow verbose"
+    exit 0
 fi
 
 # ensure workflow name passed
-validate_params "${FLAGS[w]}" "Workflow"
+validate_params "$workflow" "workflow"
 
+# add completion
+complete -o bashdefault -o default -F _cli_completion cli.sh
 # check workflow name exists
-if [ ! -f "workflows/$workflow_name.yaml" ]; then
-    echo "Error: Invalid workflow name '$workflow_name'."
+if [ ! -f "workflows/${workflow}.yaml" ]; then
+    echo -e "Error: Invalid workflow name '${workflow}'."
     exit 1
 fi
 
-# run workflow 
-if [ $verbose ]; then 
-    echo "Running $workflow_name workflow verbose"
-fi
-
-source "workflows/$workflow_name.yaml"
+source "workflows/${workflow}.yaml"
 
 
